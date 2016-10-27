cc162d25ef01b751b9724a8825933053dc44bc3b
Reason_Song <song.reason@gmail.com>
2016-10-26 21:35:26 -0400

    A5 project added.

---
+++ A5/src/gui/Main.java
@@ -0,0 +1,12 @@
+/* NetId(s): rs2352, hy483. Time spent: hh hours, mm minutes. */
+
+package gui;
+
+import model.NotImplementedException;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		throw new NotImplementedException();
+	}
+}
