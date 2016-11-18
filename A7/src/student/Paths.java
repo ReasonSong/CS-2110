a9fa2523a348bc1a431a5c20d057696738a4eba9
Reason_Song <song.reason@gmail.com>
2016-11-17 21:18:32 -0500

    A7 implemented and tested.

--- A7/src/student/Paths.java
+++ A7/src/student/Paths.java
@@ -1,9 +1,9 @@
-/* Time spent on a7:  01 hours and 20 minutes.
+/* Time spent on a7:  01 hours and 22 minutes.
 
- * Name: Ruochen Song, Hongshu Ye
- * Netid: rs2352, hy483
+ * Name: Ruochen Song
+ * Netid: rs2352
  * What I thought about this assignment:
- *
+ * TOO EASY, should combine this with A6.
  *
  */
 
@@ -23,7 +23,7 @@ import heap.Heap;
 /** This class contains Dijkstra's shortest-path algorithm and some other methods. */
 public class Paths {
 
-	/** Return a list of the nodes on the shortest path from start to 
+    /** Return a list of the nodes on the shortest path from start to
      * end, or the empty list if a path does not exist.
      * Note: The empty list is NOT "null"; it is a list with 0 elements. */
     public static List<Node> shortestPath(Node start, Node end) {
@@ -32,7 +32,7 @@ public class Paths {
          for lecture 20.
          In particular, a min-heap (as implemented in assignment A6) should be
          used for the frontier set. We provide a declaration of the frontier.
-         
+
          We will make our solution to min-heap available as soon as the deadline
          for submission has passed.
 
@@ -41,7 +41,7 @@ public class Paths {
          that node, as described in the A7 handout, along with the length of the
          shortest path (thus far, for nodes in the frontier set). For this
          purpose, we provide static class SFdata.
-         
+
          You have to declare the HashMap field for and describe carefully what it
          means. WRITE A PRECISE DEFINITION OF IT.
 
@@ -55,44 +55,39 @@ public class Paths {
             2. e.getOther(n): n must be one of the Nodes of Edge e.
                               Return the other Node.
             3. e.length:    Return the length of Edge e.
-		
+
          Method pathDistance uses one more: n1.getEdge(n2)
          */
-    	
-    	//The HashMap local variable stores the entries for nodes in S or F as keys
-    	//and the corresponding SFdate of the nodes as values
-    	HashMap<Node, SFdata> map = new HashMap<Node, SFdata>(); 
-    	Heap<Node> frontier = new Heap<Node>();
-    	frontier.add(start, 0.0);
-    	SFdata sfd = new SFdata(0, null);
-    	map.put(start, sfd);
-    	
-    	while(frontier.size() != 0) {
-    		Node f = (Node) frontier.poll();
-    		if (f.equals(end)) {
-    			return constructPath(end, map);
-    		}
-    		
-    		List<Edge> edge = f.getExits();
-    		for (Edge e : edge) {
-    			Node w = e.getOther(f);
-    			if(!map.containsKey(w)){
-    				int distance = map.get(f).distance + e.length;
-    				frontier.add(w, (double) distance);
-    				SFdata data = new SFdata(distance, f);
-    				map.put(w, data);
-    			} else { 
-    				if (map.get(f).distance + e.length < map.get(w).distance) {
-    					int distance = map.get(f).distance + e.length;
-    					map.get(w).distance = distance;
-    					map.get(w).backPointer = f;
-    					frontier.changePriority(w, (double) distance);
-    				}
-    			}
-    		}
-    	}
-    	
-        return new LinkedList<Node>(); // no path found
+
+		 HashMap<Node, SFdata> nodeMap = new HashMap<Node, SFdata>();
+		 Heap<Node> frontierSet = new Heap<>();
+
+		 frontierSet.add(start, 0);
+		 nodeMap.put(start, new SFdata(0, null));
+
+		 while(frontierSet.size() != 0 && frontierSet.peek() != end){
+
+			 Node head = frontierSet.poll();
+			 List<Edge> edgeList = head.getExits();
+
+			 for(Edge curEdge: edgeList){
+
+				 Node otherNode = curEdge.getOther(head);
+				 int totalLength = nodeMap.get(head).distance + curEdge.length;
+
+				 if (!nodeMap.containsKey(otherNode)) {
+					 SFdata data = new SFdata(totalLength, head);
+					 nodeMap.put(otherNode, data);
+					 frontierSet.add(otherNode, totalLength);
+				 } else if (totalLength < nodeMap.get(otherNode).distance) {
+					 nodeMap.get(otherNode).distance = totalLength;
+					 nodeMap.get(otherNode).backPointer = head;
+					 frontierSet.changePriority(otherNode, totalLength);
+				 }
+			 }
+		 }
+		 if (nodeMap.containsKey(end)) return constructPath(end, nodeMap);
+		 return new LinkedList<Node>(); // no path found
     }
 
     /** Return the path from the start node to node end.
@@ -109,7 +104,7 @@ public class Paths {
         }
         return path;
     }
-    
+
     /** Return the sum of the weights of the edges on path path. */
     public static int pathDistance(List<Node> path) {
         if (path.size() == 0) return 0;
@@ -126,7 +121,7 @@ public class Paths {
             return s;
         }
     }
-    
+
     /** An instance contains information about a node: the previous node
      *  on a shortest path from the start node to this node and the distance
      *  of this node from the start node. */
@@ -140,7 +135,7 @@ public class Paths {
             distance= d;     // Distance from start node to this one.
             backPointer= p;  // Backpointer on the path (null if start node)
         }
-        
+
         /** return a representation of this instance. */
         public String toString() {
             return "dist " + distance + ", bckptr " + backPointer;
